869b5cf56a903d7e6b2cac4a8d0fee7437451f94
Shubhshree22 <55087689+Shubhshree22@users.noreply.github.com>
2020-01-23 16:09:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php include_once("home.html"); ?>
+<?php
+    $myPDO = new PDO('pgsql:host=ec2-23-21-13-88.compute-1.amazonaws.com;dbname=d3foju9tci8mh4', 'asjjetcaygqmyt', '96d22ad7da0a999ffa24ca88306db22cc966438f5dea656af2d175beef93a8f3');
+?>
