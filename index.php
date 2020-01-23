89772fee402d1213ff064a2aec95d67ab717fe37
Shubhshree22 <55087689+Shubhshree22@users.noreply.github.com>
2020-01-23 16:29:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $db = pg_connect("host=ec2-23-21-13-88.compute-1.amazonaws.com port=5432 dbname=d3foju9tci8mh4 user=asjjetcaygqmyt password=96d22ad7da0a999ffa24ca88306db22cc966438f5dea656af2d175beef93a8f3");
-$query = "INSERT INTO book VALUES ('$_POST[firstname]','$_POST[lastname]')";
+$query = "INSERT INTO user_rec VALUES ('$_POST[firstname]','$_POST[lastname]')";
 $result = pg_query($query); 
 ?>
 
