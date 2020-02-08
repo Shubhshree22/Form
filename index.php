2a88439506b81b9d4fe78c206a90e21d6fe5e59a
Shubhshree22 <55087689+Shubhshree22@users.noreply.github.com>
2020-02-08 11:46:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include_once("home.html"); ?>
 <?php
-$db = pg_connect("host=ec2-23-21-13-88.compute-1.amazonaws.com port=5432 dbname=d3foju9tci8mh4 user=asjjetcaygqmyt password=96d22ad7da0a999ffa24ca88306db22cc966438f5dea656af2d175beef93a8f3");
+$db = pg_connect("host=ec2-3-213-192-58.compute-1.amazonaws.com port=5432 dbname=d7l2kkp3dbh528 user=vukesdjwuboehi password=87fe14c4854fc8bfbea10ae0fcd8a8a12cf2e47296d118fcea47f40bc363017b");
 $query = "INSERT INTO user_rec_table VALUES ('$_POST[firstname]','$_POST[lastname]')";
 $result = pg_query($query); 
 ?>
